b2188ad4cb1c0457961d82c5b48700e3ed18202b
David Jenn <david_jenn@insideranken.org>
2022-10-16 04:01:55 -0500

    update url

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import io from 'socket.io-client'
-const URL = 'http://localhost:5000';
+const URL = 'https://talk-rooms-server-david-jenn.herokuapp.com/';
+//https://talk-rooms-server-david-jenn.herokuapp.com/  http://localhost:5000
 
 export const socket = io(URL, {
   withCredentials: true,
